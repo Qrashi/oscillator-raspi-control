a59f816644c01243e5f97dbfe8e3e5dece0addbd
BuildTools <unconfigured@null.spigotmc.org>
2023-07-02 00:12:33 +0200

    fix always using git pulling

--- controller.py
+++ controller.py
@@ -47,18 +47,18 @@ def initialize():
             if not code.stdout.decode('utf-8').endswith(" "):
                 # Update found
                 display.center(2, "downloading [2/2]")
-            context.task = "git pull"
-            code = run("git pull", stdout=PIPE, stderr=PIPE, shell=True)
-            if code.returncode != 0:
-                display.center(2, "dl error")
-                report_error("Could not pull git updates! code: " + str(code.returncode),
-                             exception="Log: stdout:\n" + str(code.stdout) + "\nstderr:\n" + str(code.stderr))
-            else:
-                context.task = "update commit"
-                if not code.stdout.decode('utf-8').endswith("up to date.\n"):
-                    display.center(2, "updated, restarting")
-                    # print("restarting!")
-                    os.execl(sys.executable, sys.executable, *sys.argv)
+                context.task = "git pull"
+                code = run("git pull", stdout=PIPE, stderr=PIPE, shell=True)
+                if code.returncode != 0:
+                    display.center(2, "dl error")
+                    report_error("Could not pull git updates! code: " + str(code.returncode),
+                                 exception="Log: stdout:\n" + str(code.stdout) + "\nstderr:\n" + str(code.stderr))
+                else:
+                    context.task = "update commit"
+                    if not code.stdout.decode('utf-8').endswith("up to date.\n"):
+                        display.center(2, "updated, restarting")
+                        # print("restarting!")
+                        os.execl(sys.executable, sys.executable, *sys.argv)
 
     display.center(1, "init (link)")
     display.center(2, "finding IP")
